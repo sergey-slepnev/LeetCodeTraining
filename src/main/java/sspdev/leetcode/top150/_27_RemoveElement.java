d55d1ba0ed3d690334b274c180cd0fb1dbb266e1
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-11-09 19:38:19 +0900

    27.RemoveElement. New solution

--- src/main/java/sspdev/leetcode/top150/_27_RemoveElement.java
+++ src/main/java/sspdev/leetcode/top150/_27_RemoveElement.java
@@ -12,7 +12,7 @@ public class _27_RemoveElement {
     public static void main(String[] args) {
         int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
         int val = 2;
-        removeElement(nums, val);
+        System.out.println(removeElement_2(nums, val));
     }
 
     private static int removeElement(int[] nums, int val) {
@@ -24,5 +24,23 @@ public class _27_RemoveElement {
             }
         }
         return index;
+    }
+
+    private static int removeElement_2(int[] nums, int val) {
+        int counter = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                counter++;
+            }
+        }
+        for (int i = 0; i < nums.length - counter; i++) {
+            if (nums[i] == val) {
+                for (int j = i; j < nums.length - 1; j++) {
+                    nums[j] = nums[j + 1];
+                }
+                i--;
+            }
+        }
+        return nums.length - counter;
     }
 }
