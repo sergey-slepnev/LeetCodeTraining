af6ca341c32d9cee7bf522caf8496ff3063c168f
sergey-slepnev <kolybelnaya27ss@gmail.com>
2023-11-23 15:15:56 +0800

    13. Roman to Integer. New solution.

--- src/main/java/sspdev/leetcode/top150/_13_RomanToInteger.java
+++ src/main/java/sspdev/leetcode/top150/_13_RomanToInteger.java
@@ -6,11 +6,12 @@ public class _13_RomanToInteger {
 
     public static void main(String[] args) {
         String roman = "MCMXCIV";
-        int result = romanToInt(roman);
+        int result = romanToInt2(roman);
         System.out.println(result);
 
     }
 
+    //        O(n), O(1)
     private static int romanToInt(String s) {
         var map = new HashMap<Character, Integer>();
         map.put('I', 1);
@@ -31,6 +32,30 @@ public class _13_RomanToInteger {
             }
         }
         return result;
-//        O(n), O(1)
+    }
+
+    //    O(n), O(1)
+    private static int romanToInt2(String s) {
+        var map = new HashMap<Character, Integer>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int result = 0;
+        int position = s.length() - 1;
+        while (position >= 0) {
+            if (position != 0 && map.get(s.charAt(position)) > map.get(s.charAt(position - 1))) {
+                result += map.get(s.charAt(position)) - map.get(s.charAt(position - 1));
+                position -= 2;
+            } else {
+                result += map.get(s.charAt(position));
+                position--;
+            }
+        }
+        return result;
     }
 }
